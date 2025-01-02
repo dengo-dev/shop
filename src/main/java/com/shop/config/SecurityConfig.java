43861aaaec24514cf2759aacfff50d2255fb2dee
dengo23-dev <dengo23.dev@gmail.com>
2025-01-02 22:48:53 +0900

    security 모두 허용 및 mainController생성

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -1,16 +1,43 @@
 package com.shop.config;
 
+import com.shop.service.MemberService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
-public class SecurityConfig{
+@EnableWebSecurity
+@Log4j2
+public class SecurityConfig  {
+  
+  @Autowired
+  MemberService memberService;
+  
+  
+  //임시로 모두 허용
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+            .requestMatchers(new AntPathRequestMatcher("/**"))
+            .permitAll()
+        );
+    return http.build();
+  }
+
+
   
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
-  }}
+  }
+  
+}
