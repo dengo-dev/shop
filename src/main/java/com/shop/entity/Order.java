2654f6940aaee87f9b7ffa5ebce796755fc92c20
dengo23-dev <dengo23.dev@gmail.com>
2025-01-03 16:36:37 +0900

    Order 엔티티,Enum 추가

---
+++ src/main/java/com/shop/entity/Order.java
@@ -0,0 +1,34 @@
+package com.shop.entity;
+
+
+import com.shop.constant.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+  
+  @Id
+  @GeneratedValue
+  @Column(name = "order_id")
+  private Long id;
+  
+  @ManyToOne
+  @JoinColumn(name = "member_id")
+  private Member member;
+  
+  private LocalDateTime orderDate;
+  
+  @Enumerated(EnumType.STRING)
+  private OrderStatus orderStatus;
+  
+  private LocalDateTime regTime;
+  
+  private LocalDateTime updateTime;
+}
