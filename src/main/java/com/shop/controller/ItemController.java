8153daedcb877729970460687683da290a450d83
dengo23-dev <dengo23.dev@gmail.com>
2025-01-03 18:02:24 +0900

    ItemController 수정 & ItemForm페이지 수정

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -1,7 +1,9 @@
 package com.shop.controller;
 
 
+import com.shop.dto.ItemFormDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,7 +11,8 @@ public class ItemController {
   
   
   @GetMapping("/admin/item/new")
-  public String itemForm() {
+  public String itemForm(Model model) {
+    model.addAttribute("itemFormDto", new ItemFormDto());
     return "/item/itemForm";
   }
 }
