f0b52ec2b37d41c1a49ce2fb42c85217dc463ad3
dengo23-dev <dengo23.dev@gmail.com>
2025-01-05 00:24:41 +0900

    주문 이력 쿼리 추가

--- src/main/java/com/shop/repository/ItemImgRepository.java
+++ src/main/java/com/shop/repository/ItemImgRepository.java
@@ -8,5 +8,7 @@ import java.util.List;
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
   
   List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
+  
+  ItemImg findByItemIdAndRepimgYn(Long itemId, String repimgYn); //대포 이미지 찾는 쿼리 메소드 추가
 
 }
