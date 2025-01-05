6d1505ef5b08c36b030dcd6469d4717cbd52d42b
dengo23-dev <dengo23.dev@gmail.com>
2025-01-05 13:24:26 +0900

    장바구니 담는 쿼리 메소드 추가

--- src/main/java/com/shop/repository/CartRepository.java
+++ src/main/java/com/shop/repository/CartRepository.java
@@ -4,5 +4,7 @@ import com.shop.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
-
+  
+  //회원 cart찾는 쿼리 메소드 추가
+  Cart findByMemberId(Long memberId);
 }
